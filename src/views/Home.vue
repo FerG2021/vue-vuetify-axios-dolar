7810dcec987d25cc28aafd2ed4d944ab08006398
Fernando Gonzalez <fernandojaviergonzalez2018@gmail.com>
2022-01-26 00:43:30 -0300

    agrego calendario y dolar

---
+++ src/views/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <v-layout> <!-- filas -->
+      <v-flex xs12>  <!-- columnas -->
+        <v-card>
+          <v-date-picker 
+          v-model="fecha"
+          full-width
+          locale="es-ar"
+          :min="minimo"
+          :max="maximo"
+          @change="getDolar(fecha)">
+
+          </v-date-picker>
+        </v-card>
+        
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">
+            {{valor}} - {{fecha}}
+          </v-card-text>
+        </v-card>
+      </v-flex>      
+    </v-layout>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import {mapMutations} from 'vuex'
+  
+  export default {
+    data(){
+      return{
+        fecha: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        minimo: "1984",
+        maximo: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        valor: null
+      }
+    },
+    methods:{
+      ...mapMutations(['mostrarLoading', 'ocultarLoading']),
+
+      async getDolar(dia){   
+        let arrayFecha = dia.split(['-']);        
+        let ddmmyyyy = arrayFecha[2] + '-' + arrayFecha[1] + '-' + arrayFecha[0]; 
+        try {
+          this.mostrarLoading({titulo: 'Accediendo a la información', color: 'secondary'})
+          let datos = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyyyy}`);
+          
+          if(datos.data.serie.length > 0){
+            console.log(datos.data.serie[0].valor);
+            this.valor = await datos.data.serie[0].valor;
+          }else{
+            this.valor = 'Sin resultados';
+            console.log(datos.data);
+          }          
+          
+        } catch (error) {
+          console.log(error);
+        }    
+        finally{
+          this.ocultarLoading();
+        }   
+        
+      }
+    },
+    created(){
+      this.getDolar(this.fecha)
+    }
+  }
+</script>
